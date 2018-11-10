370a5a97c0eb751acdc83cff3ad72da678a2fad8
Eric Ahnell <eric.ahnell@puttysoftware.com>
2018-11-10 16:02:34 -0500

    Convert to Maven dependencies.
    Also remove now-redundant code.

--- twisted-trek/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module studio.ignitionigloogames.twistedtrek {
-    requires java.desktop;
-
-    provides javax.sound.sampled.spi.AudioFileReader
-	    with com.github.trilarion.sound.vorbis.sampled.spi.VorbisAudioFileReader;
-    provides javax.sound.sampled.spi.FormatConversionProvider
-	    with com.github.trilarion.sound.vorbis.sampled.spi.VorbisFormatConversionProvider;
-}
